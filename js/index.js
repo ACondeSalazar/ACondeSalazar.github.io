04e4ea0b8899dae633714cfa13d5a75ac8cb963d
ACondeSalazar <57960186+ACondeSalazar@users.noreply.github.com>
2024-04-08 19:16:15 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function InitializePage(){
 
-    new TypeIt("#central_text",{strings : ["Bienvenue sur mon site !"], speed : 50, startDelay:100}).go();
+    new TypeIt("#central_text",{strings : ["Ce site est toujours en construction..."], speed : 50, startDelay:100}).go();
 
     new TypeIt("#up_arrow",{strings : ['<i class="fa fa-arrow-up" aria-hidden="true"></i>'], speed : 50, startDelay:3000, cursor:false}).go();
 }
